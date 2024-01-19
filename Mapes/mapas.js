2b3fea5bf2c7b99467568dc57608ed2dfa9a93ae
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2024-01-19 15:17:26 +0100

    pupush

--- Mapes/mapas.js
+++ Mapes/mapas.js
@@ -9,8 +9,31 @@ let div = document.createElement("div");
 div.textContent = comarca;
 div.style = " width: 100px; height: 50px;background-color: #3498db; color: #fff;border: 1px solid #2980b9; border-radius: 5px;margin: 5px;"
 div.setAttribute("draggable", "true");
+div.addEventListener("dragstart", dragStart);
 let dop = document.getElementById("opcions");
 dop.appendChild(div);
 }
+let dragged = null;
+function dragStart(event) {
+    let dragged = event.target;
+}
 
-addEventListener("dragstart",)
+addEventListener("dragover", (event) => {
+    event.preventDefault();
+});
+
+addEventListener("dragenter", (event) => {
+    event.target.classList.add("dragover");
+});
+
+addEventListener("dragleave", (event) => {
+    event.target.classList.remove("dragover");
+});
+
+addEventListener("drop", (event) => {
+    if (verd) {
+        event.preventDefault();
+        dragged.parentNode.removeChild(dragged);
+        event.target.appendChild(dragged);
+    }
+});
